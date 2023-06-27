33191fc7be9387e607731a6348d643cd30514a9e
DarrenMannuela <darren.mannuela@gmail.com>
2023-06-28 00:56:51 +0700

    update on the patch method

--- make_api_handler.py
+++ make_api_handler.py
@@ -60,6 +60,16 @@ def get_description(table_name: str):
 
     return field_names
 
+def insert_description(table_name: str):
+    field_names = ''
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM '+table_name)
+    for i in cursor.description:
+        if i[0] != "ID":
+            field_names += i[0].lower()+", "
+
+    return field_names
+
 
 def get_attr(table_name: str):
     abbreviation = "&"
@@ -98,15 +108,22 @@ def get_num_bind (table_name: str):
     cursor.execute(f"SELECT COUNT(*) FROM all_tab_columns WHERE table_name = '{table_name.upper()}'")
     num_columns = cursor.fetchone()[0]
 
-    bind = fill_bind(num_columns)
+    bind = fill_bind(num_columns-1)
 
     return bind
 
+def get_num_col (table_name: str):
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT COUNT(*) FROM all_tab_columns WHERE table_name = '{table_name.upper()}'")
+    num_columns = cursor.fetchone()[0]
+
+
+    return num_columns-1
 
 
 
 def make_get_sql(table_name: str):
-    get = f'rows, err := db.Query("SELECT * FROM {table_name}")'+"""
+    get = f'rows, err := db.Query(`SELECT * FROM {table_name}`)'+"""
     if err != nil{
         return c.Status(500).SendString(err.Error())
     }"""
@@ -114,17 +131,42 @@ def make_get_sql(table_name: str):
 
 
 def make_get_sql_by_id(table_name: str):
-    get = f'rows, err := db.Query("SELECT * FROM {table_name} WHERE id = :1", id)'+"""
+    get = f'rows, err := db.Query(`SELECT * FROM {table_name} WHERE id = :1`, id)'+"""
     if err != nil{
         return c.Status(500).SendString(err.Error())
     }"""
     return get
 
+def make_id_exist(table_name: str):
+    get = f"""
+    var idExist string
+    err = db.QueryRow(`SELECT * FROM {table_name} WHERE id = :1`, id).Scan(&idExist)"""+"""
+    if err != nil{
+        tx.Rollback()
+		return c.Status(400).SendString("ID does not exist")
+    }"""
+    return get
 
+
+def make_id_exist_del(workpsace: str):
+    get = f"""
+    var idExist string
+	_ = tx.QueryRow(`SELECT {workpsace}id FROM {workpsace}workspace WHERE {workpsace}id = :1`, id).Scan(&idExist)
+	if idExist != "" """+"{"+f"""
+		_, err = tx.Exec(`DELETE FROM {workpsace}workspace WHERE {workpsace}id = :1`, id)
+		if err != nil""" + "{"+"""
+			tx.Rollback()
+			return c.Status(500).SendString(err.Error())
+		}
+
+	}
+    """
+    return get
 
 
 def make_insert_sql(table_name: str, desc: str, bind: str, attr: str):
-    insert = f"_, err = tx.Exec(`INSERT INTO {table_name} ({desc[:-2]}) VALUES ({bind[:-2]})`, {attr[:-2]})"+"""
+
+    insert = f"_, err = tx.Exec(`INSERT INTO {table_name} ({desc[:-2]}) VALUES ({bind[:-2]}) RETURNING id INTO :{get_num_col(table_name)+1}`, {attr[:-2]}"+", sql.Out{Dest: &generatedID})"+"""
     if err != nil {"""+f"""
         tx.Rollback()
         fmt.Println({table_name.upper()})
@@ -133,19 +175,82 @@ def make_insert_sql(table_name: str, desc: str, bind: str, attr: str):
     return insert
 
 
+def make_update_sql(table_name: str, desc: str, bind: str, attr: str):
+    combine = ""
+
+    bind_list = bind.split(',')
+    desc_list = desc.split(',')
+
+    bind_list = bind_list[:-1]
+    desc_list = desc_list[:-1]
+
+    for bind in range(len(bind_list)):
+        if bind < len(bind_list)-1:
+           combine += desc_list[bind]+" = "+bind_list[bind].replace(" ", "")+","
+        else:
+            combine += desc_list[bind]+" = "+bind_list[bind].replace(" ", "")
+    
+
+    update = f"""_, err = tx.Exec(`UPDATE {table_name} SET
+        {combine} WHERE id = :{get_num_col(table_name)+1}`, {attr}id)"""+"""
+        if err != nil {"""+f"""
+            tx.Rollback()
+            fmt.Println({table_name.upper()})
+            return err"""+"""
+        }"""
+    return update
+
+
+def make_get_by_afe(table_name: str):
+    get = f"""
+    var idExist string
+    err = db.QueryRow(`SELECT {table_name}id FROM {table_name}table WHERE afe_number = :1`, id).Scan(&idExist)"""+"""
+    if err != nil{
+        tx.Rollback()
+		return c.Status(400).SendString("ID does not exist")
+    }"""
+    return get
+
+def make_patch_sql(insert_attr: str):
+    insert_attr_list = insert_attr.split(",")
+
+    patch = ""
+    
+    for attr in insert_attr_list:
+        get_column = attr.split(".")
+        get_field = attr.replace(" ", "")
+        get_field = get_field[1:]
+
+        if len(get_column) > 1:
+            field_name = get_column[1].lower()
+            cur_if = f"""
+        if {get_field} != nil"""+"{"+f"""
+             _, err = tx.Exec(`UPDATE {table_name} SET {field_name} = :1 WHERE id = :2`, {get_field}, id)
+        """+"""
+            if err != nil {
+                tx.Rollback()
+                return c.Status(500).SendString(err.Error())
+            }
+        }
+            """
+            patch += cur_if
+    return patch
 
 def make_type_handler(table_name: str, import_name: str, func_name: str, struct_name: str):
     abbreviation = ""
+    workspace = ""
     table_name_list =  table_name.split("_")
     for name in range(len(table_name_list)):
         cur_word = table_name_list[name]
         abbreviation += cur_word[0].lower()
 
-
-    
+        if "table" not in cur_word:
+            workspace += cur_word.lower()+"_"
 
     attr = get_attr(table_name)
     insert = insert_attr(table_name)
+
+    insert_desc = insert_description(table_name)
 
     check_validity = """validity, err := utils.CheckValidity(c)
     if err != nil {
@@ -165,7 +270,7 @@ def make_type_handler(table_name: str, import_name: str, func_name: str, struct_
     "github.com/gofiber/fiber/v2"
 )
 """
-    get_all = f"""func Get{func_name}(c *fiber.Ctx) error"""+"{"+f"""
+    get= f"""func Get{func_name}(c *fiber.Ctx) error"""+"{"+f"""
     {check_validity}"""+f"""
     {make_get_sql(table_name)}\n
     """+f"""
@@ -184,13 +289,7 @@ def make_type_handler(table_name: str, import_name: str, func_name: str, struct_
     return c.JSON(results)
 }
 """
-
-
-
-    # get_by_id = f"func Get{func_name}ById(c *fiber.Ctx) error"+"{"+"""\n
-    #   {check_validity}
-    # """
-    set_all = f"""func Set{func_name}(c *fiber.Ctx) error"""+"{"+f"""
+    set = f"""func Set{func_name}(c *fiber.Ctx) error"""+"{"+f"""
     {check_validity}"""+f"""
     var {abbreviation} dto.{struct_name}
     setDefaults(&{abbreviation})
@@ -199,7 +298,7 @@ def make_type_handler(table_name: str, import_name: str, func_name: str, struct_
         return err
     }
     """+f"""
-    {abbreviation}.Create_date = formatDateString(printWellReport.Create_date)
+    {abbreviation}.Create_date = formatDateString({abbreviation}.Create_date)
     """+"""
 
 	tx, err := db.Begin()
@@ -208,12 +307,12 @@ def make_type_handler(table_name: str, import_name: str, func_name: str, struct_
 	}
 	defer func() {
 		if err != nil {
-			tx.Rollback()
+			return c.Status(500).SendString(err.Error())
 		}
 	}()
 
     var generatedID int64    
-    """+f"""{make_insert_sql(table_name, get_description(table_name), get_num_bind(table_name), insert)}"""+"""
+    """+f"""{make_insert_sql(table_name, insert_desc, get_num_bind(table_name), insert)}"""+"""
     return c.SendStatus(fiber.StatusOK)
 }
 """
@@ -240,8 +339,9 @@ def make_type_handler(table_name: str, import_name: str, func_name: str, struct_
 }
 """
     
-    update_all = f"""func Set{func_name}(c *fiber.Ctx) error"""+"{"+f"""
+    update = f"""func Put{func_name}(c *fiber.Ctx) error"""+"{"+f"""
     {check_validity}"""+f"""
+    id := c.Params("id")
     var {abbreviation} dto.{struct_name}
     setDefaults(&{abbreviation})
 
@@ -249,7 +349,7 @@ def make_type_handler(table_name: str, import_name: str, func_name: str, struct_
         return err
     }
     """+f"""
-    {abbreviation}.Create_date = formatDateString(printWellReport.Create_date)
+    {abbreviation}.Create_date = formatDateString({abbreviation}.Create_date)
     """+"""
 
 	tx, err := db.Begin()
@@ -258,26 +358,103 @@ def make_type_handler(table_name: str, import_name: str, func_name: str, struct_
 	}
 	defer func() {
 		if err != nil {
-			tx.Rollback()
+			return c.Status(500).SendString(err.Error())
 		}
 	}()
+    """+f"""
+    {make_id_exist(table_name)}
+    """+"""
+    if idExist != "" {
 
-    var generatedID int64    
-    """+f"""{make_insert_sql(table_name, get_description(table_name), get_num_bind(table_name), insert)}"""+"""
+    """+f"""
+        {make_update_sql(table_name, insert_desc, bind, insert)}
+    """+"""
+    }
+
+    if err := tx.Commit(); err != nil {
+		tx.Rollback()
+		return c.Status(500).SendString(err.Error())
+	}
+    
+    """+"""
     return c.SendStatus(fiber.StatusOK)
 }
 """
 
-    return header+get_all+set_all+get_by_id
+    delete = f"""func Delete{func_name}(c *fiber.Ctx) error"""+"{"+f"""
+    {check_validity}"""+f"""
+    id := c.Params("id")"""+"""
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+	}()
+    """+f"""
+    {make_id_exist_del(workspace)}
+    _, err = tx.Exec(`DELETE FROM {table_name} WHERE id = :1`, id)
+	if err != nil"""+"{"+"""
+		tx.Rollback()
+		return c.Status(500).SendString(err.Error())
+	}
+	err = tx.Commit()
+	if err != nil {
+		return c.Status(500).SendString(err.Error())
+	}
+    """+"""
+    return c.SendStatus(fiber.StatusOK)
+}
+"""
+    patch = f"""func Patch{func_name}(c *fiber.Ctx) error"""+"{"+f"""
+    {check_validity}"""+f"""
+    id := c.Params("id")
+    var {abbreviation} dto.{struct_name}
+    setDefaults(&{abbreviation})
+
+    if err := c.BodyParser(&{abbreviation}); err != nill"""+"{"+"""
+        return err
+    }
+    """ + """
+    tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+	}()
+    """+f"""
+    {make_get_by_afe(workspace)}
+    """+"""
+    if idExist != ""{"""+f"""{make_patch_sql(insert)}"""+"""
+    }
+    err = tx.Commit()
+	if err != nil {
+		return c.Status(500).SendString(err.Error())
+	}
+
+	return c.JSON(map[string]string{"message": "Record updated successfully"})
+}
+
+    """
+
+    return header+get+set+get_by_id+update+delete+patch
 
 
 test = "print_well_report_table"
-desc = get_description(test)
+desc = insert_description(test)
 bind = get_num_bind(test)
-attr = get_attr(test)
+attr =insert_attr(test)
 
 
-testing = make_type_handler(test, 'printwellreporttable', "PrintWellReport", "Print_well_report")
+testing = make_type_handler(test, 'printwellreport', "PrintWellReport", "Print_well_report")
+
+make_update_sql(test, desc, bind, attr)
+
 
 
 with open(f"test.go", 'w') as file:
--- test.go
+++ test.go
@@ -5,8 +5,8 @@ import (
     "fmt"
     "strconv"
 
-    dto "github.com/DarrenMannuela/svc-datatype-printwellreporttable/dto"
-    "github.com/DarrenMannuela/svc-datatype-printwellreporttable/utils"
+    dto "github.com/DarrenMannuela/svc-datatype-printwellreport/dto"
+    "github.com/DarrenMannuela/svc-datatype-printwellreport/utils"
 
     "github.com/gofiber/fiber/v2"
 )
@@ -16,7 +16,7 @@ func GetPrintWellReport(c *fiber.Ctx) error{
         return c.Status(validity).SendString(err.Error())
     }
     
-    rows, err := db.Query("SELECT * FROM print_well_report_table")
+    rows, err := db.Query(`SELECT * FROM print_well_report_table`)
     if err != nil{
         return c.Status(500).SendString(err.Error())
     }
@@ -49,7 +49,7 @@ func SetPrintWellReport(c *fiber.Ctx) error{
         return err
     }
     
-    pwrt.Create_date = formatDateString(printWellReport.Create_date)
+    pwrt.Create_date = formatDateString(pwrt.Create_date)
     
 
 	tx, err := db.Begin()
@@ -58,12 +58,12 @@ func SetPrintWellReport(c *fiber.Ctx) error{
 	}
 	defer func() {
 		if err != nil {
-			tx.Rollback()
+			return c.Status(500).SendString(err.Error())
 		}
 	}()
 
     var generatedID int64    
-    _, err = tx.Exec(`INSERT INTO print_well_report_table (id, ba_long_name, ba_type, area_id, area_type, field_name, well_name, uwi, title, creator_name, create_date, media_type, document_type, item_category, item_sub_category, page_count, remark, data_store_name, data_store_type, source, qc_status, checked_by_ba_id) VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, :14, :15, :16, :17, :18, :19, :20, :21, :22)`, &pwrt.Ba_long_name, &pwrt.Ba_type, &pwrt.Area_id, &pwrt.Area_type, &pwrt.Field_name, &pwrt.Well_name, &pwrt.Uwi, &pwrt.Title, &pwrt.Creator_name, &pwrt.Create_date, &pwrt.Media_type, &pwrt.Document_type, &pwrt.Item_category, &pwrt.Item_sub_category, &pwrt.Page_count, &pwrt.Remark, &pwrt.Data_store_name, &pwrt.Data_store_type, &pwrt.Source, &pwrt.Qc_status, &pwrt.Checked_by_ba_id)
+    _, err = tx.Exec(`INSERT INTO print_well_report_table (ba_long_name, ba_type, area_id, area_type, field_name, well_name, uwi, title, creator_name, create_date, media_type, document_type, item_category, item_sub_category, page_count, remark, data_store_name, data_store_type, source, qc_status, checked_by_ba_id) VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, :14, :15, :16, :17, :18, :19, :20, :21) RETURNING id INTO :22`, &pwrt.Ba_long_name, &pwrt.Ba_type, &pwrt.Area_id, &pwrt.Area_type, &pwrt.Field_name, &pwrt.Well_name, &pwrt.Uwi, &pwrt.Title, &pwrt.Creator_name, &pwrt.Create_date, &pwrt.Media_type, &pwrt.Document_type, &pwrt.Item_category, &pwrt.Item_sub_category, &pwrt.Page_count, &pwrt.Remark, &pwrt.Data_store_name, &pwrt.Data_store_type, &pwrt.Source, &pwrt.Qc_status, &pwrt.Checked_by_ba_id, sql.Out{Dest: &generatedID})
     if err != nil {
         tx.Rollback()
         fmt.Println(PRINT_WELL_REPORT_TABLE)
@@ -79,7 +79,7 @@ func GetPrintWellReportById(c *fiber.Ctx) error{
     
     id := c.Params("id")
     
-    rows, err := db.Query("SELECT * FROM print_well_report_table WHERE id = :1", id)
+    rows, err := db.Query(`SELECT * FROM print_well_report_table WHERE id = :1`, id)
     if err != nil{
         return c.Status(500).SendString(err.Error())
     }
@@ -99,3 +99,332 @@ func GetPrintWellReportById(c *fiber.Ctx) error{
     
     return c.JSON(results)
 }
+func PutPrintWellReport(c *fiber.Ctx) error{
+    validity, err := utils.CheckValidity(c)
+    if err != nil {
+        return c.Status(validity).SendString(err.Error())
+    }
+    
+    id := c.Params("id")
+    var pwrt dto.Print_well_report
+    setDefaults(&pwrt)
+
+    if err := c.BodyParser(&pwrt); err != nill{
+        return err
+    }
+    
+    pwrt.Create_date = formatDateString(pwrt.Create_date)
+    
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+	}()
+    
+    
+    var idExist string
+    err = db.QueryRow(`SELECT * FROM print_well_report_table WHERE id = :1`, id).Scan(&idExist)
+    if err != nil{
+        tx.Rollback()
+		return c.Status(400).SendString("ID does not exist")
+    }
+    
+    if idExist != "" {
+
+    
+        _, err = tx.Exec(`UPDATE print_well_report_table SET
+        ba_long_name = :1, ba_type = :2, area_id = :3, area_type = :4, field_name = :5, well_name = :6, uwi = :7, title = :8, creator_name = :9, create_date = :10, media_type = :11, document_type = :12, item_category = :13, item_sub_category = :14, page_count = :15, remark = :16, data_store_name = :17, data_store_type = :18, source = :19, qc_status = :20, checked_by_ba_id = :21 WHERE id = :22`, &pwrt.Ba_long_name, &pwrt.Ba_type, &pwrt.Area_id, &pwrt.Area_type, &pwrt.Field_name, &pwrt.Well_name, &pwrt.Uwi, &pwrt.Title, &pwrt.Creator_name, &pwrt.Create_date, &pwrt.Media_type, &pwrt.Document_type, &pwrt.Item_category, &pwrt.Item_sub_category, &pwrt.Page_count, &pwrt.Remark, &pwrt.Data_store_name, &pwrt.Data_store_type, &pwrt.Source, &pwrt.Qc_status, &pwrt.Checked_by_ba_id, id)
+        if err != nil {
+            tx.Rollback()
+            fmt.Println(PRINT_WELL_REPORT_TABLE)
+            return err
+        }
+    
+    }
+
+    if err := tx.Commit(); err != nil {
+		tx.Rollback()
+		return c.Status(500).SendString(err.Error())
+	}
+    
+    
+    return c.SendStatus(fiber.StatusOK)
+}
+func DeletePrintWellReport(c *fiber.Ctx) error{
+    validity, err := utils.CheckValidity(c)
+    if err != nil {
+        return c.Status(validity).SendString(err.Error())
+    }
+    
+    id := c.Params("id")
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+	}()
+    
+    
+    var idExist string
+	_ = tx.QueryRow(`SELECT print_well_report_id FROM print_well_report_workspace WHERE print_well_report_id = :1`, id).Scan(&idExist)
+	if idExist != "" {
+		_, err = tx.Exec(`DELETE FROM print_well_report_workspace WHERE print_well_report_id = :1`, id)
+		if err != nil{
+			tx.Rollback()
+			return c.Status(500).SendString(err.Error())
+		}
+
+	}
+    
+    _, err = tx.Exec(`DELETE FROM print_well_report_table WHERE id = :1`, id)
+	if err != nil{
+		tx.Rollback()
+		return c.Status(500).SendString(err.Error())
+	}
+	err = tx.Commit()
+	if err != nil {
+		return c.Status(500).SendString(err.Error())
+	}
+    
+    return c.SendStatus(fiber.StatusOK)
+}
+func PatchPrintWellReport(c *fiber.Ctx) error{
+    validity, err := utils.CheckValidity(c)
+    if err != nil {
+        return c.Status(validity).SendString(err.Error())
+    }
+    
+    id := c.Params("id")
+    var pwrt dto.Print_well_report
+    setDefaults(&pwrt)
+
+    if err := c.BodyParser(&pwrt); err != nill{
+        return err
+    }
+    
+    tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+	}()
+    
+    
+    var idExist string
+    err = db.QueryRow(`SELECT print_well_report_id FROM print_well_report_table WHERE afe_number = :1`, id).Scan(&idExist)
+    if err != nil{
+        tx.Rollback()
+		return c.Status(400).SendString("ID does not exist")
+    }
+    
+    if idExist != ""{
+        if pwrt.Ba_long_name != nil{
+             _, err = tx.Exec(`UPDATE  SET ba_long_name = :1 WHERE id = :2`, pwrt.Ba_long_name, id)
+        
+            if err != nil {
+                tx.Rollback()
+                return c.Status(500).SendString(err.Error())
+            }
+        }
+            
+        if pwrt.Ba_type != nil{
+             _, err = tx.Exec(`UPDATE  SET ba_type = :1 WHERE id = :2`, pwrt.Ba_type, id)
+        
+            if err != nil {
+                tx.Rollback()
+                return c.Status(500).SendString(err.Error())
+            }
+        }
+            
+        if pwrt.Area_id != nil{
+             _, err = tx.Exec(`UPDATE  SET area_id = :1 WHERE id = :2`, pwrt.Area_id, id)
+        
+            if err != nil {
+                tx.Rollback()
+                return c.Status(500).SendString(err.Error())
+            }
+        }
+            
+        if pwrt.Area_type != nil{
+             _, err = tx.Exec(`UPDATE  SET area_type = :1 WHERE id = :2`, pwrt.Area_type, id)
+        
+            if err != nil {
+                tx.Rollback()
+                return c.Status(500).SendString(err.Error())
+            }
+        }
+            
+        if pwrt.Field_name != nil{
+             _, err = tx.Exec(`UPDATE  SET field_name = :1 WHERE id = :2`, pwrt.Field_name, id)
+        
+            if err != nil {
+                tx.Rollback()
+                return c.Status(500).SendString(err.Error())
+            }
+        }
+            
+        if pwrt.Well_name != nil{
+             _, err = tx.Exec(`UPDATE  SET well_name = :1 WHERE id = :2`, pwrt.Well_name, id)
+        
+            if err != nil {
+                tx.Rollback()
+                return c.Status(500).SendString(err.Error())
+            }
+        }
+            
+        if pwrt.Uwi != nil{
+             _, err = tx.Exec(`UPDATE  SET uwi = :1 WHERE id = :2`, pwrt.Uwi, id)
+        
+            if err != nil {
+                tx.Rollback()
+                return c.Status(500).SendString(err.Error())
+            }
+        }
+            
+        if pwrt.Title != nil{
+             _, err = tx.Exec(`UPDATE  SET title = :1 WHERE id = :2`, pwrt.Title, id)
+        
+            if err != nil {
+                tx.Rollback()
+                return c.Status(500).SendString(err.Error())
+            }
+        }
+            
+        if pwrt.Creator_name != nil{
+             _, err = tx.Exec(`UPDATE  SET creator_name = :1 WHERE id = :2`, pwrt.Creator_name, id)
+        
+            if err != nil {
+                tx.Rollback()
+                return c.Status(500).SendString(err.Error())
+            }
+        }
+            
+        if pwrt.Create_date != nil{
+             _, err = tx.Exec(`UPDATE  SET create_date = :1 WHERE id = :2`, pwrt.Create_date, id)
+        
+            if err != nil {
+                tx.Rollback()
+                return c.Status(500).SendString(err.Error())
+            }
+        }
+            
+        if pwrt.Media_type != nil{
+             _, err = tx.Exec(`UPDATE  SET media_type = :1 WHERE id = :2`, pwrt.Media_type, id)
+        
+            if err != nil {
+                tx.Rollback()
+                return c.Status(500).SendString(err.Error())
+            }
+        }
+            
+        if pwrt.Document_type != nil{
+             _, err = tx.Exec(`UPDATE  SET document_type = :1 WHERE id = :2`, pwrt.Document_type, id)
+        
+            if err != nil {
+                tx.Rollback()
+                return c.Status(500).SendString(err.Error())
+            }
+        }
+            
+        if pwrt.Item_category != nil{
+             _, err = tx.Exec(`UPDATE  SET item_category = :1 WHERE id = :2`, pwrt.Item_category, id)
+        
+            if err != nil {
+                tx.Rollback()
+                return c.Status(500).SendString(err.Error())
+            }
+        }
+            
+        if pwrt.Item_sub_category != nil{
+             _, err = tx.Exec(`UPDATE  SET item_sub_category = :1 WHERE id = :2`, pwrt.Item_sub_category, id)
+        
+            if err != nil {
+                tx.Rollback()
+                return c.Status(500).SendString(err.Error())
+            }
+        }
+            
+        if pwrt.Page_count != nil{
+             _, err = tx.Exec(`UPDATE  SET page_count = :1 WHERE id = :2`, pwrt.Page_count, id)
+        
+            if err != nil {
+                tx.Rollback()
+                return c.Status(500).SendString(err.Error())
+            }
+        }
+            
+        if pwrt.Remark != nil{
+             _, err = tx.Exec(`UPDATE  SET remark = :1 WHERE id = :2`, pwrt.Remark, id)
+        
+            if err != nil {
+                tx.Rollback()
+                return c.Status(500).SendString(err.Error())
+            }
+        }
+            
+        if pwrt.Data_store_name != nil{
+             _, err = tx.Exec(`UPDATE  SET data_store_name = :1 WHERE id = :2`, pwrt.Data_store_name, id)
+        
+            if err != nil {
+                tx.Rollback()
+                return c.Status(500).SendString(err.Error())
+            }
+        }
+            
+        if pwrt.Data_store_type != nil{
+             _, err = tx.Exec(`UPDATE  SET data_store_type = :1 WHERE id = :2`, pwrt.Data_store_type, id)
+        
+            if err != nil {
+                tx.Rollback()
+                return c.Status(500).SendString(err.Error())
+            }
+        }
+            
+        if pwrt.Source != nil{
+             _, err = tx.Exec(`UPDATE  SET source = :1 WHERE id = :2`, pwrt.Source, id)
+        
+            if err != nil {
+                tx.Rollback()
+                return c.Status(500).SendString(err.Error())
+            }
+        }
+            
+        if pwrt.Qc_status != nil{
+             _, err = tx.Exec(`UPDATE  SET qc_status = :1 WHERE id = :2`, pwrt.Qc_status, id)
+        
+            if err != nil {
+                tx.Rollback()
+                return c.Status(500).SendString(err.Error())
+            }
+        }
+            
+        if pwrt.Checked_by_ba_id != nil{
+             _, err = tx.Exec(`UPDATE  SET checked_by_ba_id = :1 WHERE id = :2`, pwrt.Checked_by_ba_id, id)
+        
+            if err != nil {
+                tx.Rollback()
+                return c.Status(500).SendString(err.Error())
+            }
+        }
+            
+    }
+    err = tx.Commit()
+	if err != nil {
+		return c.Status(500).SendString(err.Error())
+	}
+
+	return c.JSON(map[string]string{"message": "Record updated successfully"})
+}
+
+    
